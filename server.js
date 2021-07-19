466b47b7c6abcd3c6c5b2131c6412de47d422ba5
lucastseun <lucastseun@qq.com>
2021-07-19 23:36:27 +0800

    修改

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import { createApp, createRouter } from './lib/mod.js'
-import logger from './src/logger/logger.js'
 
 const app = createApp()
 const router = createRouter()
 
-app.use(logger())
 
 app.use(router.routes())
+router.get('/user', ctx=>{
+    console.log(1111);
+})
+router.get('/title', ctx=>{
+    console.log(2222);
+})
 
 app.listen({port: 3000}, () => {
     console.log('server run at: http://localhost:3000');
--- src/logger/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = () => {
-    return (ctx) => {
-        console.log(`${ctx.req.method}---${ctx.req.url}---${ctx.res.statusCode}`);
-    }
-}
-
-export default logger
